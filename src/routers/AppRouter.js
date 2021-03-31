319dfa6dbe3c254afea9bce7bf1a0213966e1b67
Daniel <danielcgilibert@gmail.com>
2021-03-31 20:09:35 +0200

    Agregado Route,Menu,datos...

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { Navbar } from "../components/ui/NavBar";
+import { DashboardRoutes } from "./DashboardRoutes";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        {/* <Navbar /> */}
+
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route path="/" component={DashboardRoutes} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
